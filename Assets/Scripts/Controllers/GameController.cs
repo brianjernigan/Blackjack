657ce615df3f669248fc557d832f2c4f89355601
brianjernigan <brobertjernigan@gmail.com>
2024-02-27 19:39:47 -0500

    Initial Game Setup
    More buttons!

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void OnClickQuitButton()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
     }
 }
